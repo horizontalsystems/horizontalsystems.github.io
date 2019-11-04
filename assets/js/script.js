a8b717412acf1af420781fef1e06b0860ddda652
Talgat Medetbekov <medetbekov.t@gmail.com>
2019-11-04 16:27:46 +0600

    Apply new design

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,32 @@
-function dropdownMenu() {
-  const lang = document.querySelector('.with-dropdown')
-  const menu = document.querySelector('.with-dropdown .nav-dropdown')
-  if (!menu) {
+function team() {
+  let members = document.querySelectorAll(".team-member");
+  let avatar = document.querySelector(".section-avatar");
+  if (avatar == null) {
     return
   }
 
-  lang.addEventListener('mouseover', function () {
-    menu.className = 'nav-dropdown active'
-  })
+  const onClick = function (element) {
+    avatar.style.backgroundImage = "url(\"" + element.getAttribute("src") + "\")";
+  };
 
-  lang.addEventListener('mouseout', function () {
-    menu.className = 'nav-dropdown'
-  })
+  members.forEach(element => {
+    element.addEventListener('click', function () {
+      onClick(element);
+
+      members.forEach(el => {
+        el.classList.remove("selected")
+      });
+
+      element.classList.add("selected");
+      let memberName = document.querySelector("#member-name");
+      if (memberName) {
+        memberName.textContent = element.getAttribute("alt")
+      }
+    })
+  });
+
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-
-})
+  team()
+});
