3e434c48d0591d7fc8a8224b6a4c50b30d3d4794
Talgat Medetbekov <medetbekov.t@gmail.com>
2019-11-06 16:49:40 +0600

    Add fancybox gallery

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,6 +27,33 @@ function team() {
 
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  team()
+function openGallery() {
+  $.fancybox.open([
+    {src: '/assets/images/gallery/4.jpg'},
+    {src: '/assets/images/gallery/1.jpg'},
+    {src: '/assets/images/gallery/2.jpg'},
+    {src: '/assets/images/gallery/3.jpg'},
+    {src: '/assets/images/gallery/5.jpg'},
+    {src: '/assets/images/gallery/6.jpg'},
+    {src: '/assets/images/gallery/7.jpg'},
+    {src: '/assets/images/gallery/8.jpg'},
+    {src: '/assets/images/gallery/9.jpg'},
+    {src: '/assets/images/gallery/10.jpg'},
+    {src: '/assets/images/gallery/11.jpg'},
+    {src: '/assets/images/gallery/12.jpg'},
+    {src: '/assets/images/gallery/13.jpg'},
+    {src: '/assets/images/gallery/14.jpg'},
+    {src: '/assets/images/gallery/15.jpg'},
+    {src: '/assets/images/gallery/16.jpg'},
+    {src: '/assets/images/gallery/17.jpg'},
+    {src: '/assets/images/gallery/18.jpg'},
+    {src: '/assets/images/gallery/19.jpg'},
+    {src: '/assets/images/gallery/20.jpg'}
+  ]);
+}
+
+$(function () {
+  team();
+
+  $("#open-gallery").on('click', openGallery);
 });
