d0041436a59b5d7d78c107f4ca3dd5c107175726
Talgat Medetbekov <medetbekov.t@gmail.com>
2020-03-30 15:00:31 +0600

    Update team image

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -17,10 +17,10 @@ function Team() {
             <div className="Edge-content-start">
               <div className="Icon-wrap">
                 <Icon name="team-new" fill="none" viewBox="0 0 52 52" size="52" />
-                <div className="Icon-label color-whites">Team</div>
+                <div className="Icon-label color-white">Team</div>
               </div>
             </div>
-            <div className="Edge-content-end Edge-content-end-text color-dark">
+            <div className="Edge-content-end Edge-content-end-text color-white">
               We believe open blockchain are at the verge of fundamentally transforming global finance.
             </div>
           </div>
