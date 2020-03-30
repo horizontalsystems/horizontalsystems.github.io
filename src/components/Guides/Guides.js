bf9cc7b75d8ae261bfd25deb62fe4256f8a3c2f4
Talgat Medetbekov <medetbekov.t@gmail.com>
2020-03-30 15:15:14 +0600

    Add preview to guides

--- src/components/Guides/Guides.js
+++ src/components/Guides/Guides.js
@@ -19,6 +19,42 @@ class Guides extends React.Component {
     this.setState({ padding: rect.left })
   }
 
+  showGuide = name => {
+    let images = []
+    switch (name) {
+      case 'libra': {
+        images = [
+          { src: '/images/libra/01.png', opts: { thumb: '/images/libra/01.png' } },
+          { src: '/images/libra/02.png', opts: { thumb: '/images/libra/02.png' } },
+          { src: '/images/libra/03.png', opts: { thumb: '/images/libra/03.png' } },
+          { src: '/images/libra/04.png', opts: { thumb: '/images/libra/04.png' } },
+          { src: '/images/libra/05.png', opts: { thumb: '/images/libra/05.png' } },
+          { src: '/images/libra/06.png', opts: { thumb: '/images/libra/06.png' } },
+          { src: '/images/libra/07.png', opts: { thumb: '/images/libra/07.png' } }
+        ]
+
+        break
+      }
+      default:
+        images = [
+          { src: '/images/btc-in-simple-term/Bitcoin-Explained-Simple-Terms-002.png', opts: { thumb: '/images/btc-in-simple-term/Bitcoin-Explained-Simple-Terms-002.png' } },
+          { src: '/images/btc-in-simple-term/Bitcoin-Explained-Simple-Terms-003.png', opts: { thumb: '/images/btc-in-simple-term/Bitcoin-Explained-Simple-Terms-003.png' } },
+          { src: '/images/btc-in-simple-term/Bitcoin-Explained-Simple-Terms-004.png', opts: { thumb: '/images/btc-in-simple-term/Bitcoin-Explained-Simple-Terms-004.png' } },
+          { src: '/images/btc-in-simple-term/Bitcoin-Explained-Simple-Terms-005.png', opts: { thumb: '/images/btc-in-simple-term/Bitcoin-Explained-Simple-Terms-005.png' } },
+          { src: '/images/btc-in-simple-term/Bitcoin-Explained-Simple-Terms-006.png', opts: { thumb: '/images/btc-in-simple-term/Bitcoin-Explained-Simple-Terms-006.png' } },
+          { src: '/images/btc-in-simple-term/Bitcoin-Explained-Simple-Terms-007.png', opts: { thumb: '/images/btc-in-simple-term/Bitcoin-Explained-Simple-Terms-007.png' } }
+        ]
+    }
+
+    // eslint-disable-next-line no-undef
+    $.fancybox.open(images, {
+      baseClass: "guides-gallery",
+      thumbs: {
+        autoStart: true
+      }
+    });
+  }
+
   render() {
     return (
       <Container className="Guides" fluid>
@@ -42,10 +78,10 @@ class Guides extends React.Component {
         <Scroller padding={this.state.padding}>
           <Card
             title="Libra Explained In Simple Terms"
-            info={<>What is Bitcoin? <br />What makes Bitcoin so unique? <br />And how can you invest? <br /></>}
+            info={<>Do you know a whale from a bear? <br />A cold wallet from a decentralised wallet?<br /></>}
             cover="/images/cards/libra.png"
             actionsLeft={
-              <a href="/">Preview</a>
+              <span onClick={() => this.showGuide('libra')}>Preview</span>
             }
           />
           <Card
@@ -53,7 +89,19 @@ class Guides extends React.Component {
             info={<>What is Bitcoin? <br />What makes Bitcoin so unique? <br />And how can you invest? <br /></>}
             cover="/images/cards/bitcoin.png"
             actionsLeft={
-              <a href="/">Preview</a>
+              <span onClick={() => this.showGuide('simple-term')}>Preview</span>
+            }
+          />
+          <Card
+            title="Crypto Terms for Beginners"
+            info={<>
+              Do you know what a whale means in crypto? <br />
+              The difference between non-custodial and custodial wallets? <br />
+              What KYC and AML mean and why you should care? <br />
+              </>}
+            cover="/images/cards/question.png"
+            actionsLeft={
+              <span>Preview</span>
             }
           />
         </Scroller>
