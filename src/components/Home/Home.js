9b23543c8c1df955c5d5c39c7c066b8050e7360c
Talgat Medetbekov <medetbekov.t@gmail.com>
2020-03-30 16:22:28 +0600

    Add background image to Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,11 +4,12 @@ import Header from '../Header/Header'
 import Banner from '../Banner/Banner'
 import Container from '../Container/Container'
 
-import './Home.css'
+import './Home.scss'
 
 function Home() {
   return (
     <Container fluid>
+      <div className="Home-half" />
       <Header />
       <Banner />
     </Container>
