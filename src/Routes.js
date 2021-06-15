58f37fdc66296b4c8d540e37983b146439000ae4
Talgat Medetbekov <medetbekov.t@gmail.com>
2021-06-15 18:40:38 +0600

    Disable Google Analytics

--- src/Routes.js
+++ src/Routes.js
@@ -4,13 +4,11 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Markdown from './components/Markdown/Markdown'
 import App from './components/App/App'
 import Modal from './components/Modal/Modal'
-import CookieConsent from './components/CookieConsent/CookieConsent'
 
 function Routes() {
   return (
     <BrowserRouter>
       <Modal />
-      <CookieConsent />
 
       <Switch>
         <Route path="/privacy" render={() => <Markdown markdown="privacy.md" />} />
